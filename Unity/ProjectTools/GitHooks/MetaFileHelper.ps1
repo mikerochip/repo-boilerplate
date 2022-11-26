4d97948e7a542713d3464f33a91835ebdd3422a8
Mike Schweitzer <mschweitzer@gmail.com>
2022-11-26 00:52:15 -0800

    Fixed bugs with Unity project GitHooks
    * Same-repo check was not checking correct paths
    * Embedded packages were not being checked correctly

--- Unity/ProjectTools/GitHooks/MetaFileHelper.ps1
+++ Unity/ProjectTools/GitHooks/MetaFileHelper.ps1
@@ -2,8 +2,8 @@
 class MetaFileHelper {
     static [string]FindGitPath([string]$path) {
         while ($path) {
-            $gitPath = Get-ChildItem $path -Hidden | Where-Object { $PSItem.Name -eq '.git' }
-            if ($gitPath) {
+            $gitPath = "$path/.git"
+            if (Test-Path $gitPath -PathType Container) {
                 return $gitPath
             }
             $path = [System.IO.Directory]::GetParent($path)
@@ -13,12 +13,12 @@ class MetaFileHelper {
 
     <#
     .SYNOPSIS
-    Returns full paths of all top-level folders that include meta files.
+    Returns full paths of all folders that include meta files for the Unity project.
     .INPUTS
-    The Unity project at the given path.
+    The Unity project path.
     .DESCRIPTION
-    Includes the 'Assets' folder. Also includes local and embedded package dependencies, but
-    only if they live in the same repo as the Unity project.
+    Includes the 'Assets' folder and embedded package folders. Also includes local packages,
+    but only if they are in the same repo as the Unity project.
     #>
     static [string[]]GetMetaFileFolderPaths([string]$unityProjectPath) {
         Write-Verbose 'GetMetaFileFolderPaths'
@@ -29,30 +29,59 @@ class MetaFileHelper {
 
         # Unity adds meta files to:
         # 1. "Assets/"
-        # 2. Any local or embedded packages in "Packages/manifest.json"
-        $folderPaths = @([System.IO.Path]::GetFullPath('Assets', $unityProjectPath))
-    
+        # 2. Embedded packages in "Packages/"
+        # 3. Local packages from "Packages/manifest.json"
+        $dirPaths = New-Object System.Collections.Generic.List[string]
+        
+        Write-Verbose "  Add Assets"
+        $dirPaths.Add([System.IO.Path]::GetFullPath('Assets', $unityProjectPath))
+
+        Write-Verbose "  Check Embedded Packages"
+        foreach ($item in Get-ChildItem "Packages" -Directory) {
+            Write-Verbose "  Check `"$item`""
+            Write-Verbose "    Test `"$item/package.json`""
+            if (Test-Path "$item/package.json" -PathType Leaf) {
+                Write-Verbose "    Include"
+                $dirPaths.Add($item.FullName)
+            } else {
+                Write-Verbose "    Skip: No `"package.json`""
+            }
+        }
+
+        $prevWorkingDirectory = [System.IO.Directory]::GetCurrentDirectory()
+        [System.IO.Directory]::SetCurrentDirectory("$unityProjectPath/Packages")
+
+        Write-Verbose "  Check Local Packages"
         $manifest = Get-Content 'Packages/manifest.json' | ConvertFrom-Json
-        $folderPaths += foreach ($property in $manifest.dependencies.PSObject.Properties) {
+        foreach ($property in $manifest.dependencies.PSObject.Properties) {
             if ($property.Value -notlike 'file:*') {
                 continue
             }
 
             $path = $property.Value -replace '^file:*', ''
             Write-Verbose "  Check `"$path`""
+            
+            $path = [System.IO.Path]::GetFullPath($path)
+            Write-Verbose "    Path `"$path`""
+
+            if ($dirPaths.Contains($path)) {
+                Write-Verbose "    Skip: Duplicate"
+                continue
+            }
 
             $dependencyGitPath = [MetaFileHelper]::FindGitPath($path)
-            Write-Verbose "  dependencyGitPath `"$dependencyGitPath`""
+            Write-Verbose "    GitPath `"$dependencyGitPath`""
 
             if ($projectGitPath -ne $dependencyGitPath) {
-                Write-Verbose "  Skip `"$path`""
+                Write-Verbose "    Skip: DifferentGitPath"
                 continue
             }
             
-            Write-Verbose "  Include `"$path`""
-            [System.IO.Path]::GetFullPath($path, $unityProjectPath)
+            Write-Verbose "    Include"
+            $dirPaths.Add($path)
         }
+        [System.IO.Directory]::SetCurrentDirectory($prevWorkingDirectory)
 
-        return $folderPaths
+        return $dirPaths.ToArray()
     }
 }
